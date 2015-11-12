23ced748d1d39fa808263c5b4f5caf4c5bf1970f
Nabil <530605@student.hin.no>
2015-11-12 20:57:17 +0100

    endring i MyUtility Class.

--- ProsjektOppgaven/testProsjektOppgaven/prosjektOppgaven/MyUtilityClassTest.java
+++ ProsjektOppgaven/testProsjektOppgaven/prosjektOppgaven/MyUtilityClassTest.java
@@ -1,15 +1,17 @@
 package prosjektOppgaven;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
 
 public class MyUtilityClassTest {
+	
+	MyUtilityClass utilityClass = new MyUtilityClass();
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-		//test
+		
+		
 	}
 
 }
