07055c0befbd33c6f62cf9b0dc917dfbf1f9a648
Nabil <530605@student.hin.no>
2015-11-13 20:18:30 +0100

    Noen metoder.

--- ProsjektOppgaven/prosjektOppgaven/prosjektOppgaven/MyClass.java
+++ ProsjektOppgaven/prosjektOppgaven/prosjektOppgaven/MyClass.java
@@ -6,6 +6,19 @@ public final class MyClass {
 	public MyClass() {
 
 	}
+	
+	public static int konverterHexTilInt(String s) {
+		if(sjekkHexStringen(s))
+			return 1;
+		return -1;
+	}
+
+	public static boolean sjekkHexStringen(String s) {
+		if(s.length() == 6) 
+			if(s.matches(".*[a-f].*") || s.matches(".*[A-F].*") || s.matches(".*[0-9].*"))
+				return true;
+		return false;
+	}
 
 	public static int isHexadecimal(String s) {
 		if (s == "a")
--- ProsjektOppgaven/testProsjektOppgaven/prosjektOppgaven/MyClassTest.java
+++ ProsjektOppgaven/testProsjektOppgaven/prosjektOppgaven/MyClassTest.java
@@ -1,6 +1,6 @@
 package prosjektOppgaven;
 
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -14,16 +14,28 @@ import static org.junit.Assert.*;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(MyClass.class)
 public class MyClassTest {
-
+	
+	@Ignore
 	@Test
-	public void isHexadecimal_Ten_ShouldBe_10() {
+	public void isHexadecimal_Ten_ShouldBe10() {
 		
 		assertEquals(MyClass.isHexadecimal("a"), 10);
-
+				
 		PowerMockito.mockStatic(MyClass.class);
 		Mockito.when(MyClass.isHexadecimal("a")).thenReturn(-1);
 
 		assertEquals(MyClass.isHexadecimal("a"), -1);
+	}
+	
+	@Test
+	public void sjekkHexStringen_03ac0f_ShouldBeTrue() {
+		
+		assertTrue(MyClass.sjekkHexStringen("03ac0f"));
+				
+		PowerMockito.mockStatic(MyClass.class);
+		Mockito.when(MyClass.sjekkHexStringen("a")).thenReturn(true);
+
+		assertTrue(MyClass.sjekkHexStringen("a"));
 	}
 
 }
